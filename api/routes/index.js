401006d5bab9c96ec9c8bdb8e264f8c0f11905f6
JoseMa <esparev@hotmail.com>
2023-03-26 19:11:19 -0600

    docs: 📝 add swagger ui docs for users, stores and payment methd

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 
 function routerApi(app) {
 	const router = express.Router();
 
-  // Parent route
+	// Parent route
 	app.use('/api/v1', router);
-  // Children routes
+	// Children routes
+	router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 }
 
 module.exports = routerApi;
